080e9af309cf19f8e9acc69ee5f0a790f9925422
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2025-04-23 19:19:22 +0200

    Create screen and use it, removing demo screen

---
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/ui/core/navigation/NavigationWrapper.kt
@@ -0,0 +1,19 @@
+package com.jmonzonm.rickmortyapp.ui.core.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jmonzonm.rickmortyapp.ui.home.HomeScreen
+
+@Composable
+fun NavigationWrapper() {
+    val mainNavController = rememberNavController()
+
+    NavHost(navController = mainNavController, startDestination = Routes.Home.route) {
+        composable(route = Routes.Home.route) {
+            HomeScreen()
+        }
+    }
+
+}
---
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/ui/home/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.jmonzonm.rickmortyapp.ui.home
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+    Text("Funcionó")
+}
