3ea28786f4137b4e894daf1b72577a80ae0f9127
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2025-04-24 21:09:40 +0200

    Create response model

---
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/data/remote/response/CharacterResponse.kt
@@ -0,0 +1,31 @@
+package com.jmonzonm.rickmortyapp.data.remote.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CharacterResponse(
+    val created: String,
+    val episode: List<String>,
+    val gender: String,
+    val id: Int,
+    val image: String,
+    val location: Location,
+    val name: String,
+    val origin: Origin,
+    val species: String,
+    val status: String,
+    val type: String,
+    val url: String
+)
+
+@Serializable
+data class Origin(
+    val name: String,
+    val url: String
+)
+
+@Serializable
+data class Location(
+    val name: String,
+    val url: String
+)
