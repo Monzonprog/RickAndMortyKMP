97665a30755506e10c73bab739900306552543bb
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2025-04-24 21:33:47 +0200

    Create model and comment params temporally

--- composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/data/remote/response/CharacterResponse.kt
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/data/remote/response/CharacterResponse.kt
@@ -1,22 +1,31 @@
 package com.jmonzonm.rickmortyapp.data.remote.response
 
+import com.jmonzonm.rickmortyapp.domain.model.CharacterModel
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class CharacterResponse(
-    val created: String,
+    /*val created: String,
     val episode: List<String>,
-    val gender: String,
+    val gender: String,*/
     val id: Int,
     val image: String,
-    val location: Location,
+    /*val location: Location,
     val name: String,
     val origin: Origin,
-    val species: String,
+    val species: String,*/
     val status: String,
-    val type: String,
-    val url: String
-)
+    /*val type: String,
+    val url: String*/
+) {
+    fun toDomain(): CharacterModel {
+        return CharacterModel(
+            id = id,
+            image = image,
+            isAlive = status.lowercase() == "alive"
+        )
+    }
+}
 
 @Serializable
 data class Origin(
---
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/domain/model/CharacterModel.kt
@@ -0,0 +1,26 @@
+package com.jmonzonm.rickmortyapp.domain.model
+
+data class CharacterModel(
+    /*val created: String,
+    val episode: List<String>,
+    val gender: String,*/
+    val id: Int,
+    val image: String,
+   /* val location: Location,
+    val name: String,
+    val origin: Origin,
+    val species: String,*/
+    val isAlive: Boolean,
+    /*val type: String,
+    val url: String*/
+)
+
+data class Origin(
+    val name: String,
+    val url: String
+)
+
+data class Location(
+    val name: String,
+    val url: String
+)
