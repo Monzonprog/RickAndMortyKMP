8675bfcddab8ee89ca058e3fc6630062fac4a91c
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2025-05-01 13:23:03 +0200

    Include paging library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -56,6 +56,8 @@ kotlin {
             implementation(libs.ktor.client.logging)
             implementation(libs.coil.compose)
             implementation(libs.coil.network.ktor3)
+            implementation(libs.paging.compose.common)
+            implementation(libs.paging.common)
         }
         iosMain.dependencies {
             implementation(libs.ktor.client.darwin)
