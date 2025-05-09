2bbd1e5bd53e1f282ebf5d244ed9d27e7629754a
Jorge Monzón <jorge.monzon.manzano@nttdata.com>
2025-05-09 20:48:32 +0200

    Include use case in view model and create state

---
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/ui/home/tabs/episodes/EpisodesState.kt
@@ -0,0 +1,10 @@
+package com.jmonzonm.rickmortyapp.ui.home.tabs.episodes
+
+import androidx.paging.PagingData
+import com.jmonzonm.rickmortyapp.domain.model.EpisodeModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class EpisodesState(
+    val episodes: Flow<PagingData<EpisodeModel>> = emptyFlow()
+)
--- composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/ui/home/tabs/episodes/EpisodesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/jmonzonm/rickmortyapp/ui/home/tabs/episodes/EpisodesViewModel.kt
@@ -1,9 +1,29 @@
 package com.jmonzonm.rickmortyapp.ui.home.tabs.episodes
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.jmonzonm.rickmortyapp.domain.GetAllEpisodesUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class EpisodesViewModel(private val getAllEpisodesUseCase: GetAllEpisodesUseCase) : ViewModel() {
 
+    private val _state = MutableStateFlow<EpisodesState>(EpisodesState())
+    val state: StateFlow<EpisodesState> = _state
+
+    init {
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                getAllEpisodesUseCase().cachedIn(viewModelScope)
+            }
+            _state.update { it.copy(episodes = result) }
+        }
+    }
 }
